f63fece8be7ce4236978141198213a7fc49ccd73
ilya-petrashkevich <ilya.petrashkevich@gmail.com>
2022-11-19 16:28:22 +0300

    samurai-way-40video-done

---
+++ src/components/Dialogs/Dialogs.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import s from './Dialogs.module.css';
+import DialogItem from "./DialogItem/DialogItem";
+import Message from "./Message/Message";
+import {DialogsType, MessagesType} from "../redux/state";
+
+type DialogsPropsType = {
+    dialogs: DialogsType[]
+    messages: MessagesType[]
+}
+
+const Dialogs = (props: DialogsPropsType) => {
+
+    let dialogsElements = props.dialogs.map(d => <DialogItem name={d.name} id={d.id}/>);
+
+    let messagesElements = props.messages.map(m => <Message message={m.message}/>);
+
+    return (
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                {dialogsElements}
+            </div>
+            <div className={s.messages}>
+                {messagesElements}
+            </div>
+        </div>
+    );
+};
+
+export default Dialogs;
